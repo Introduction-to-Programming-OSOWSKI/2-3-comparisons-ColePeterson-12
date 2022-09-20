5b2c28cf95f6f1d55431be02b95a4f2d821832dc
ColePeterson-12 <816427@mystma.org>
2022-09-20 15:15:43 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
 #WRITE YOUR CODE IN THIS FILE
+from tkinter import Y
+
+
+def greaterThan(x,y):
+   if x>y:
+        return True
+    else:
+        return False
+
+def lesserThan(x,y):
+    if x<Y:
+        return True
+    else:
+        return False
+
+def equalTo(x,y):
+    if x==y:
+        return True
+    else:
+        return False
+
+def greaterOrEqual(x,y):
+    if x>=y:
+        return True
+    else:
+        return False
+def lessOrEqual(x,y):
+    if x<=y:
+        return True
+    else:
+        return False
+
+
